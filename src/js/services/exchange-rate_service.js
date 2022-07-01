2cb666484e50f3875e900b8d84c7c36e751ec301
Kyle Chinick <kylechinick@icloud.com>
2022-07-01 09:20:12 -0700

    Update getCurrentExchangeRate() to test with USD to EUR

--- src/js/services/exchange-rate_service.js
+++ src/js/services/exchange-rate_service.js
@@ -2,7 +2,7 @@ export default class ExchangeRateService {
   static async getCurrentExchangeRate(amountToConvert) {
     try {
       const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/EUR/GBP/${amountToConvert}`
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/EUR/${amountToConvert}`
       );
       if (!response.ok) {
         throw Error(response.statusText);
